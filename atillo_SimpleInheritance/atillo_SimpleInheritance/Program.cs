4a67469e4e936985aebf9523a4f20aee31f13145
ACT-STUDENT <ACT-STUDENT@LAB3-PC33>
2025-09-01 14:51:48 +0800

    hahahaha

---
+++ atillo_SimpleInheritance/atillo_SimpleInheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atillo_SimpleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //John Rhaezel C. Atillo
+            //Section: IT-306
+            //EXER_06: Simple Inheritance
+
+            //Create an Instance of the Student class
+            Student student = new Student
+            {
+                Name = "John Rhaezel C. Atillo",
+                Program = "BSIT"
+            };
+            student.BasicInfo(); //Call the method to display student info
+
+            //Create an instance of the Irregular student class
+            RegularStudent regularStudent = new RegularStudent
+            {
+                Name = "Jack Antony Yagonia",
+                Program = "BSIT"
+            };
+            student.BasicInfo();
+            student.
+            
+
+        }
+    }
+}
---
+++ atillo_SimpleInheritance/atillo_SimpleInheritance/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atillo_SimpleInheritance
+{
+    internal class Student
+    {
+        //Properties for the base class
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        //Method to display student information
+        public void BasicInfo()
+        {
+            Console.WriteLine("\n-----Student Information-----\n");
+            Console.WriteLine($"Name: {Name}, \nProgram: {Program}");
+        }
+    }
+
+    class RegularStudent : Student //Derived Class
+    {
+        //Additional Property for the Regular Student class
+        public string Section { get; set; }
+
+        //Method to display Regular Student class
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"Section: {Section}");
+        }
+    }
+
+    class IrregularStudent : Student //another derived class
+    {
+        //Additional Property for the Irregular Student class
+        public string UnitsEnrolled { get; set; }
+
+        //Method to display Irregular Student class
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine($"Units Enrolled: {UnitsEnrolled}");
+        }
+        
+    }
+
+}
+    
