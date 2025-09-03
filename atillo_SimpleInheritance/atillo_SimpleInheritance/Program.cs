f3ff6e5ce39a2276d87920aae1b8fab942d27cec
John Rhaezel C. Atillo <johnrhaezelatillo@gmail.com>
2025-09-03 14:18:38 +0800

    Update Program.cs

--- atillo_SimpleInheritance/atillo_SimpleInheritance/Program.cs
+++ atillo_SimpleInheritance/atillo_SimpleInheritance/Program.cs
@@ -6,32 +6,34 @@ using System.Threading.Tasks;
 
 namespace atillo_SimpleInheritance
 {
-    internal class Program
+       internal class Program
     {
         static void Main(string[] args)
         {
+
             //John Rhaezel C. Atillo
             //Section: IT-306
             //EXER_06: Simple Inheritance
-
-            //Create an Instance of the Student class
-            Student student = new Student
-            {
-                Name = "John Rhaezel C. Atillo",
-                Program = "BSIT"
-            };
-            student.BasicInfo(); //Call the method to display student info
 
             //Create an instance of the Irregular student class
             RegularStudent regularStudent = new RegularStudent
             {
                 Name = "Jack Antony Yagonia",
-                Program = "BSIT"
+                Program = "BSIT",
+                Section = "IT306"
             };
-            student.BasicInfo();
-            student.
-            
+            regularStudent.BasicInfo();
+            regularStudent.SectionEnrolled();
 
+
+            IrregularStudent irreggularstudent = new IrregularStudent
+            {
+                Name = "Okay",
+                Program = "BSIT",
+                UnitsEnrolled = "20 Units"
+            };
+            irreggularstudent.BasicInfo();
+            irreggularstudent.EnrolledSemUnits();
         }
     }
 }
