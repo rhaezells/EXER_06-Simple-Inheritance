a2a48cd370c2bd4dbea95875a92c1efdd0fc043f
ACT-STUDENT <ACT-STUDENT@LAB3-PC33>
2025-09-12 14:38:25 +0800

    file

---
+++ EXER_08 Inheritance with User Input/EXER_08 Inheritance with User Input/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_08_Inheritance_with_User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //John Rhaezel C. Atillo
+            //IT 306
+            //EXER_08: Inheritance with User Input
+
+            Console.WriteLine("-----Choose what type of shoes you want----");
+            Console.WriteLine("1. Running Shoes");
+            Console.WriteLine("2. Sports Shoes");
+            Console.WriteLine("---Enter your choice 1 or 2----");
+            string choice = Console.ReadLine();
+
+
+            if (choice == "1")
+            {
+                Console.WriteLine("---Enter the Sneakers Brand:");
+                string Brand = Console.ReadLine();
+
+                Console.WriteLine("---Enter size---:");
+                int Size = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("---Enter SportType---");
+                string SportType = Console.ReadLine();
+
+                //Show info
+                Sneakers sneakers = new Sneakers(Brand, Size, SportType);
+                sneakers.BasicInfo();
+                sneakers.SneakerInfo();
+                Console.WriteLine("----Thank you for Buying----");
+            }
+
+
+            else if (choice == "2")
+            {
+                Console.WriteLine("---Enter the Boots Brand:");
+                string Brand = Console.ReadLine();
+
+                Console.WriteLine("---Enter size---:");
+                int Size = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("---Enter BootType---");
+                string BootType = Console.ReadLine();
+
+                //Show info
+                Sneakers sneakers = new Sneakers(Brand, Size, BootType);
+                sneakers.BasicInfo();
+                sneakers.SneakerInfo();
+                Console.WriteLine("----Thank you for Buying----");
+            }
+
+
+
+            else
+            {
+                Console.WriteLine("----Please input 1 for sneakers or 2 for boots----");
+            }
+
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ EXER_08 Inheritance with User Input/EXER_08 Inheritance with User Input/Shoe.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_08_Inheritance_with_User_Input
+{
+    internal class Shoe
+    {
+        //Fields
+        public string Brand;
+        public int Size;
+
+        //Constructor
+        public Shoe(string aBrand, int aSize)
+        {
+            Brand = aBrand;
+            Size = aSize;
+        }
+
+        //Method to display
+        public void BasicInfo()
+        {
+            Console.WriteLine("----Shoe Details----");
+            Console.WriteLine($"Brand Name: {Brand}, Size: {Size}");
+        }
+
+    }
+
+    class Sneakers : Shoe
+    {
+        //Field
+        public string SportType;
+
+        //Constuctor
+        public Sneakers(string aBrand, int aSize, string aSportType) : base(aBrand, aSize)
+        {
+            SportType = aSportType;
+        }
+
+        //Method
+        public void SneakerInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Size: {Size}, Sport Type: {SportType}");
+        }
+    }
+
+    class Boots : Shoe
+    {
+        //Field
+        public string BootType;
+
+        //Constructor
+        public Boots(string aBrand, int aSize, string aBootType) : base(aBrand, aSize)
+        {
+            BootType = aBootType;
+        }
+
+        //Method
+        public void BootsInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Size: {Size}, Boot Type: {BootType}");
+        }
+    }
+}
